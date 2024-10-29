0f62cbdd46bfab07620dee37f88de4a4df0b1ae2
Harish Aerate <harish.ar@avaamo.com>
2024-10-29 19:07:10 +0530

    Schema validations added

--- src/app.js
+++ src/app.js
@@ -5,20 +5,67 @@ const userModel = require("./models/user");
 
 app.use(express.json());
 
+//Store user details to DB
 app.post("/signup", async (req, res) => {
-  console.log("req", req.body);
-
   const user = new userModel(req.body);
-
   try {
     await user.save();
     res
       .status(200)
       .send({ statusText: "User created successfully.", data: user });
-  } catch {
-    res.status(500).send({ statusText: "Unable to create user data." });
+  } catch(err) {
+    res.status(500).send({ statusText: "Unable to create user data.", data: err });
   }
 });
+
+//Get User with specific details
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try{
+    let users = await userModel.find({emailId: userEmail});
+    console.log("user", users);
+    if(users && users.length){
+      res
+    .status(200)
+    .send({ statusText: "Found User successfully.", "response": users});
+    } 
+    else res.status(404).send({ statusText: "User not found.", "response": users});
+    
+  }catch{
+    res.status(500).send({ statusText: "Something went wrong." });
+  }
+})
+
+//Get all Feeds API
+app.get("/feed", async (req, res) => {
+  try{
+    const users = await userModel.find({});
+    console.log("users from feed api", users);
+    if(users && users.length){
+      res
+    .status(200)
+    .send({ statusText: "Found User successfully.", "response": users});
+    }
+    else res.status(404).send({ statusText: "User not found.", "response": users});
+  }catch{
+    res.status(500).send({ statusText: "Something went wrong." });
+  }
+})
+
+//API to delete a user based on ID
+// app.delete("/user", async (req, res) => {
+//   const userId = req.body.userId;
+//   try{
+//     const user = await userModel.findByIdAndDelete({})
+//   }catch{
+
+//   }
+// })
+
+//Update user API 
+app.patch("/user", (req, res) => {
+  
+})
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,45 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required: true
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    lowercase: true,
+    trim: true,
+    required: true,
+    unique: true
   },
   password: {
     type: String,
+    required: true
   },
   age: {
     type: Number,
   },
   gender: {
     type: String,
+    validate(value){
+      if(!["male", "female", "others"].includes(value)){
+        throw new Error("Invalid gender");
+      }
+    }
   },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "This is a default about section"
+  },
+  skills: {
+    type: [String],
+  }
+},{
+  timestamps: true
 });
 
 const userModel = mongoose.model("User", userSchema);
